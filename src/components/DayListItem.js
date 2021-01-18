a914dd3d19ef43bc5f7b5ad7fd760fe7d29f98d9
Wwong154 <feanor4d@gmail.com>
2021-01-18 15:52:34 -0500

    added day / dalist and completed button as per assignment

---
+++ src/components/DayListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classNames from 'classnames';
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  function formatSpots(spots) {
+    return spots === 0 ? "no spots remaining" : (spots === 1 ? "1 spot remaining" : spots + " spots remaining"); 
+  }
+
+  let spotsLeft = formatSpots(props.spots);
+  let liClass = classNames('day-list__item', {"day-list__item--selected": props.selected}, {"day-list__item--full" : props.spots === 0});
+
+  return (
+    <li className={liClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsLeft}</h3>
+    </li>
+  );
+}
